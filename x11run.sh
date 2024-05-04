8cfc2df5de20cf2b24c32b08979a044566696f61
Joe Ferreira Scholtz <joefscholtz@gmail.com>
2024-05-04 16:46:13 -0300

    add gitconfig synchronization

--- x11run.sh
+++ x11run.sh
@@ -4,6 +4,6 @@ SCRIPT_PATH=$(dirname "$0")
 DOCKER_USER="ros"
 docker run -it \
 	--user $DOCKER_USER --network=host --ipc=host -v $SCRIPT_PATH/ros2_ws:/home/$DOCKER_USER/ros2_ws \
-	-v $HOME/dotfiles/:/dotfiles/ \
+	-v $HOME/.gitconfig:/home/$DOCKER_USER/.gitconfig \
 	-e DISPLAY=$DISPLAY \
 	ros2_ws
