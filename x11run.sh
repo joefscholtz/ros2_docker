af0878610fb7964ca803ac2b3b7bc7db49e960e4
Joe Ferreira Scholtz <joefscholtz@gmail.com>
2024-05-07 18:56:12 -0300

    limit container cpu and memory usage

--- x11run.sh
+++ x11run.sh
@@ -5,5 +5,7 @@ DOCKER_USER="ros"
 docker run -it \
 	--user $DOCKER_USER --network=host --ipc=host -v $SCRIPT_PATH/ros2_ws:/home/$DOCKER_USER/ros2_ws \
 	-v $HOME/.gitconfig:/home/$DOCKER_USER/.gitconfig \
+	--cpus "4" \
+	--memory "2.5g" \
 	-e DISPLAY=$DISPLAY \
 	ros2_docker:dev
